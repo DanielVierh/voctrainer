0eb2e94bffd3567fb35ab5d1279d259fa1a1bcbb
DanielVierh <DanielVierh@users.noreply.github.com>
2022-03-09 06:21:18 +0100

    Mockup für Sprachmenue erstellt und verbunden

--- src/views/LangOverview.vue
+++ src/views/LangOverview.vue
@@ -1,13 +1,35 @@
 <template>
-  
+    <div>
+        <h1>Englisch</h1>
+        <div class="buttonContainer">
+            <button class="menueButton">Neues Wort</button>
+            <button class="menueButton">Karteikarten</button>
+            <button class="menueButton">Schreibtest</button>
+            <button class="menueButton">Verwalten</button>
+        </div>
+    </div>
 </template>
 
 <script>
 export default {
-
-}
+    components: {},
+    setup() {
+        return {};
+    },
+};
 </script>
 
 <style>
-
+.menueButton {
+    width: 150px;
+    padding: 1rem;
+    margin-bottom: 8px;
+    border-radius: 15px;
+    font-family: sans-serif;
+    font-size: 1rem;
+    font-weight: bold;
+    cursor: pointer;
+    box-shadow: 5px 5px 15px black, inset 3px 3px 5px white;
+    background: rgb(255, 196, 4);
+}
 </style>
